989c07c84dd8f4cc8cf56320e0f066f94e5f84c1
Kirill Beskhitrov <beskhitrov@gmail.com>
2022-03-31 21:26:37 +0300

    init.el

---
+++ init.el
@@ -0,0 +1,121 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+;;; https://melpa.org/
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+;(package-refresh-contents)
+;(package-install 'use-package)
+
+;;; General
+
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+(setq auto-save-default nil)
+(delete-selection-mode t)
+
+;;; https://www.nordtheme.com/
+
+(use-package nord-theme
+  :ensure t
+  :config (load-theme 'nord t))
+
+;;; Интерфейс
+
+(menu-bar-mode -1)
+(line-number-mode -1)
+
+(setq display-time-format "⌚%X")
+(setq display-time-interval 1)
+(display-time)
+
+;;; https://github.com/seagle0128/doom-modeline
+
+(use-package doom-modeline
+  :ensure t
+  :config (doom-modeline-mode 1))
+
+;;; ido-mode
+
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode t)
+
+;;; https://www.flycheck.org/
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;;; https://github.com/company-mode/company-mode
+
+(use-package company
+  :ensure t
+  :init (global-company-mode))
+
+;;; https://github.com/joaotavora/yasnippet
+
+(use-package yasnippet
+  :ensure t)
+
+;;; https://github.com/AndreaCrotti/yasnippet-snippets
+
+(use-package yasnippet-snippets
+  :ensure t)
+
+;;; https://github.com/johnmastro/react-snippets.el
+
+(use-package react-snippets
+  :ensure t)
+
+;;; prog-mode
+
+(defun my-prog-mode-hook ()
+  "Hook for 'prog-mode."
+  (display-line-numbers-mode))
+    
+(add-hook 'prog-mode-hook 'my-prog-mode-hook)
+
+;;; https://github.com/smihica/emmet-mode
+
+(use-package emmet-mode
+  :ensure t
+  :hook  ((sgml-mode . emmet-mode)
+	  (css-mode . emmet-mode)
+	  (js-mode . emmet-mode))
+  :config (setq emmet-indent-after-insert nil))
+
+;;; https://github.com/emacs-lsp/lsp-mode
+
+(use-package lsp-mode
+  :ensure t
+  :init (setq
+	 lsp-keymap-prefix "C-c l"
+	 lsp-headerline-breadcrumb-enable nil)
+  :hook ((html-mode . lsp)
+	 (css-mode . lsp)
+	 (js-mode . lsp)
+	 (lsp-mode . lsp-enable-which-key-integration)
+	 (lsp-mode . yas-minor-mode))
+  :commands lsp)
+
+;;; https://github.com/justbur/emacs-which-key
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+;;; https://github.com/prettier/prettier-emacs
+
+(use-package prettier-js
+  :ensure t
+  :hook (js-mode . prettier-js-mode))
+
+(provide 'init)
+
+;;; init.el ends here
