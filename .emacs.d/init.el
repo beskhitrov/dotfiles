65d79499bbd7935b205358ea2c183498a082bd5f
Kirill Beskhitrov <beskhitrov@gmail.com>
2022-04-05 14:23:46 +0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -176,16 +176,3 @@
 (provide 'init)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(use-package diminish yasnippet-snippets xterm-color which-key vscode-icon vscode-dark-plus-theme vscdark-theme vs-dark-theme treemacs react-snippets prettier-js npm-mode nord-theme lsp-mode json-mode flycheck emmet-mode doom-themes doom-modeline company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
