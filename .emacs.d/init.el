ebf5022ac47ba9602d4db041598051b9bb5564a0
Kirill Beskhitrov <beskhitrov@gmail.com>
2022-07-26 15:18:13 +0300

    26.07.22

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -255,6 +255,31 @@
 
 (use-package react-snippets)
 
+(use-package tree-sitter
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
+
+(use-package typescript-mode
+  :after tree-sitter
+  :config
+  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
+  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+
+  ;; use our derived mode for tsx files
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  ;; by default, typescript-mode is mapped to the treesitter typescript parser
+  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
 ;; General
 
 (set-locale-environment "ru_RU.UTF-8")
@@ -282,7 +307,7 @@
       		   '(font . "BlexMono Nerd Font-16"))
       (scroll-bar-mode -1)
       (tool-bar-mode -1)
-      (toggle-frame-fullscreen)
+      ;; (toggle-frame-fullscreen)
       (setq inhibit-startup-message t
 	    inhibit-startup-echo-area-message t))
   (progn
@@ -306,7 +331,7 @@
 (defun my-term ()
   "Start Z shell."
   (interactive)
-  (ansi-term "/usr/local/bin/zsh"))
+  (ansi-term "zsh"))
 
 (global-set-key (kbd "C-c t") 'my-term)
 
